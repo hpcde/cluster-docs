0ed9c004218a49ab145b8c16db4bceb90e97b0b5
genshen <genshenchu@gmail.com>
2019-09-07 10:13:47 +0800

    docs: update header copyright comments in source files.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,8 +1,6 @@
 /**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * Copyright (c) 2017-present, HPCDE lab.
+ * 
  */
 
 // See https://docusaurus.io/docs/site-config for all the possible
